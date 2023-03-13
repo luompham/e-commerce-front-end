a29f528da20e3a51bb5add4069079457cb8bdd16
Luom Pham <luompham95@gmail.com>
2023-03-14 06:08:04 +0700

    finished product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactStars from "react-rating-stars-component";
+import { Link } from 'react-router-dom';
 
 const ProductCard = () => {
   return (
     <div className="col-3">
-      <div className="product-card position-relative">
+      <Link className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
+          <Link>
+            <img src="images/wish.svg" alt="wishlist" />
+          </Link>
+        </div>
         <div className="product-image">
           <img src="images/watch.jpg" alt="product image" />
+          <img src="images/watch-01.jpg" alt="product image" />
         </div>
         <div className="product-details">
           <h6 className="brand">Havels</h6>
@@ -17,12 +24,25 @@ const ProductCard = () => {
             count={5}
             size={24}
             value={3}
+            edit={false}
             activeColor="#ffd700"
           />
           <p className="price">$100.00</p>
         </div>
-        <div className="action-bar position-absolute"></div>
-      </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link>
+              <img src="images/prodcompare.svg" alt="compare" />
+            </Link>
+            <Link>
+              <img src="images/view.svg" alt="view" />
+            </Link>
+            <Link>
+              <img src="images/add-cart.svg" alt="add cart" />
+            </Link>
+          </div>
+        </div>
+      </Link>
     </div>
   )
 }
