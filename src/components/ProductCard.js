b4b0b0a9ef483eeba4cdc9f378008f883c6a5b8e
Luom Pham <luompham95@gmail.com>
2023-03-16 17:48:17 +0700

    our store- finished products list

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -32,6 +32,7 @@ const ProductCard = (props) => {
               edit={false}
               activeColor="#ffd700"
             />
+            <p className={`description ${grid === 12 ? 'd-block' : 'd-none'}`}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt...</p>
             <p className="price">$100.00</p>
           </div>
           <div className="action-bar position-absolute">
@@ -72,6 +73,7 @@ const ProductCard = (props) => {
               edit={false}
               activeColor="#ffd700"
             />
+            <p className={`description ${grid === 12 ? 'd-block' : 'd-none'}`}>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt...</p>
             <p className="price">$100.00</p>
           </div>
           <div className="action-bar position-absolute">
