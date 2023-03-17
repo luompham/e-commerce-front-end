05fdbd466cc68bab98f7e536664fc2dcbbe641e0
Luom Pham <luompham95@gmail.com>
2023-03-17 16:00:18 +0700

    Compare Product page - continue

--- src/pages/CompareProduct.js
+++ src/pages/CompareProduct.js
@@ -16,6 +16,18 @@ const CompareProduct = () => {
                 <div className="product-card-image">
                   <img src="images/watch.jpg" alt="" />
                 </div>
+                <div className="compare-product-details">
+                  <h5 className="title">
+                    Honor T1 7.0 1 GB RAM 8GB ROM 7 Inch With Wi-Fi+3G Tablet
+                  </h5>
+                  <h6 className="price">$ 100</h6>
+                  <div>
+                    <div className='product-detail'>
+                      <h5 className='mb-0'>Brand:</h5>
+                      <p className='mb-0'>Havels</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
